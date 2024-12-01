227d0b73ed7066feac2602876981adc05c881343
ParadoxV5 <paradox.ver5@gmail.com>
2024-12-01 13:36:53 -0700

    01.2

---
+++ 01 Historian Hysteria/main2.rb
@@ -0,0 +1,10 @@
+left, right = ARGF # .each_line
+  .map(&:split) # rows of columns
+  .transpose #: [Array[String], Array[String]] # columns of rows
+
+# Index the right list
+# * Somehow my left list is all unique numbers,
+#   So I don’t index it, assuming it’s intended to be mostly if not all unique.
+# * Don’t multiply yet, assuming the right set is not a subset of the left
+right_index = right.tally
+puts left.sum { right_index[_1]&.* _1.to_i or 0 }
