92bee8ea890f345b3c431d8dfc1e63415dc65c12
ParadoxV5 <paradox.ver5@gmail.com>
2024-12-04 13:55:05 -0700

    04.1

---
+++ 04 Ceres Search/main1.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+# Noob: Iterate every row, column, diagonal and backward
+# Pro:  Look for neighboring Xs, Ms, As & Ss
+
+# keep newlines so both `line[-1]` and `line[line.chomp.size]` gives a non-‘XMAS’ char
+input = ARGF.readlines
+# and have a similar padding for rows too
+input << ''
+
+steps = [
+  [+1,  0],
+  [+1, +1],
+  [ 0, +1],
+  [-1, +1],
+  [-1,  0],
+  [-1, -1],
+  [ 0, -1],
+  [+1, -1]
+] #: Array[[Integer, Integer]]
+
+puts input.each_with_index.sum {|row, y0|
+  row.size.times.sum do|x0|
+    steps.count do|dx, dy|
+      x, y = x0, y0
+      'XMAS'.each_char.all? do|chr|
+        if input.fetch(y)[x] == chr
+          x += dx
+          y += dy
+        end
+      end
+    end
+  end
+}
