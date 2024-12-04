e4164f041a94c1b1e634077643f00c5f37e4bc34
ParadoxV5 <paradox.ver5@gmail.com>
2024-12-03 17:34:32 -0700

    03.2 Regexp-only solution

---
+++ 03 Mull It Over/main2-regexp.rb
@@ -0,0 +1,25 @@
+# What is challenging though is building an Regexp that looks around
+# to eliminate all disabled false-positives; yet by Santa, I did it!
+puts ARGF.read #: String
+  .scan(%r{
+      (
+        # Match `…don't()…do()`
+        don't\(\)
+        .*?
+        do\(\)
+      |
+        # Match nothing since last match
+        \G
+      )
+      # Then match as long as it doesn’t have `don't()` (and `mul(…)`) in it
+      ((?~
+        don't\(\)
+        |
+        mul\(\d++,\d++\)
+      ))
+      mul\((\d++),(\d++)\)
+    }mx) #: Array[[String, String, String, String]]
+  .sum {|dont_do, skip, a, b|
+    p dont_do, skip if $VERBOSE
+    Integer(a) * Integer(b)
+  }
