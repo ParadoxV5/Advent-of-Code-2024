d8743fedd4b31e5d498fced4b14cc64cd6671e0c
ParadoxV5 <paradox.ver5@gmail.com>
2025-01-25 15:24:10 -0700

    19.2

---
+++ 19 Linen Layout/main2.rb
@@ -0,0 +1,13 @@
+towels = readline('').scan(/\w++/) #: Array[String]
+
+# Memoization cache with some pre-allocated capacity
+permutations = Hash.new(capacity: towels.size**3) do|hash, new_design| #$ String, Integer, String, Integer
+  hash[new_design] = towels.sum do|prefix_design|
+    subdesign = new_design.delete_prefix prefix_design
+    subdesign.size < new_design.size ? hash[subdesign] : 0
+  end
+end
+#towels.each { permutations[it] = 1 } # only if no towel patterns overlap, which isn’t this day
+permutations[''] = 1
+
+puts ARGF.each_line(chomp: true).sum(&permutations)
